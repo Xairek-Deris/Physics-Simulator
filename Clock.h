0d5f8460793fdf663270dae845f69957158dbe4b
Xairek-Deris <xairek@outlook.com>
2021-03-25 15:59:40 -0400

    (hopefully) finished up the clock class, adding  stop, resume, and start functionality

--- Clock.h
+++ Clock.h
@@ -1,38 +1,64 @@
 #include <chrono>
 
-class Clock
+namespace phys
 {
-public:
-    Clock()
+class Clock
     {
-        m_start_point = std::chrono::high_resolution_clock::now();
-        m_lap_point = m_start_point;
-    }
+    public:
+        Clock()
+        : m_paused{ false }
+        {
+            m_start_point = std::chrono::high_resolution_clock::now();
+            m_lap_point = m_start_point;
+        }
 
-    double Time()
-    {
-        auto end_point = std::chrono::high_resolution_clock::now();
-        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_start_point).time_since_epoch().count();
-        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(end_point).time_since_epoch().count();
-        return (end - start) * 1e-6;
-    }
+        void Start()
+        {
+            m_start_point = std::chrono::high_resolution_clock::now();
+            m_lap_point = m_start_point;
+        }
 
-    double Lap()
-    {
-        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_lap_point).time_since_epoch().count();
-        m_lap_point = std::chrono::high_resolution_clock::now();
-        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(m_lap_point).time_since_epoch().count();
-        return (end - start) * 1e-6;
-    }
-    /*
-    double Pause()
-    {
-        m_pause_point = std::chrono::high_resolution_clock::now();
-    }
-    */
+        double Stop()
+        {
+            m_stop_point = std::chrono::high_resolution_clock::now();
+            m_paused = true;
+            auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_start_point).time_since_epoch().count();
+            auto end = std::chrono::time_point_cast<std::chrono::microseconds>(m_stop_point).time_since_epoch().count();
+            return (end - start) * 1e-6;
+        }
 
-private:
-    std::chrono::time_point< std::chrono::high_resolution_clock> m_start_point;
-    std::chrono::time_point< std::chrono::high_resolution_clock> m_lap_point;
-    //std::chrono::time_point< std::chrono::high_resolution_clock> m_pause_point;
-};
+        void Resume()
+        {
+            auto current_time = std::chrono::high_resolution_clock::now();
+            auto duration = current_time - m_stop_point;
+            m_start_point += duration;
+            m_lap_point += duration;
+            m_paused = false;
+        }
+
+        double Time()
+        {
+            if(!m_paused)
+                m_stop_point = std::chrono::high_resolution_clock::now();
+            auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_start_point).time_since_epoch().count();
+            auto end = std::chrono::time_point_cast<std::chrono::microseconds>(m_stop_point).time_since_epoch().count();
+            return (end - start) * 1e-6;
+        }
+
+        double Lap()
+        {
+            if(!m_paused)
+                m_stop_point = std::chrono::high_resolution_clock::now();
+            auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_lap_point).time_since_epoch().count();
+            auto end = std::chrono::time_point_cast<std::chrono::microseconds>(m_stop_point).time_since_epoch().count();
+            m_lap_point = m_stop_point;
+            return (end - start) * 1e-6;
+        }
+
+    private:
+        std::chrono::time_point< std::chrono::high_resolution_clock> m_start_point;
+        std::chrono::time_point< std::chrono::high_resolution_clock> m_lap_point;
+        std::chrono::time_point< std::chrono::high_resolution_clock> m_stop_point;
+        bool m_paused;
+    };
+}
