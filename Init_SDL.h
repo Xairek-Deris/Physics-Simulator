29a7fc0e22426d0013cd61889ea11515e991d45c
Xairek-Deris <xairek@outlook.com>
2021-03-26 15:07:00 -0400

    Separeted init function from the window header

---
+++ Init_SDL.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+namespace disp
+{
+    int Init()
+{
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    return 0;
+}
+}
