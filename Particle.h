1489ca7f7fba09509789199f681e43b5677e90be
Xairek-Deris <xairek@outlook.com>
2021-03-30 11:05:52 -0400

    replaced texture pointer with index for a texture vector

--- Particle.h
+++ Particle.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#include <SDL2/SDL.h>
-
 #include "Vec.h"
 
 namespace phys
 {
 	struct Particle
 	{
-		Particle(const Vector& pos, const Vector& vel, double mas, double cha, double rad, double har, SDL_Texture* tex)
+		Particle(const Vector& pos, const Vector& vel, double mas, double cha, double rad, double har, int tex)
 			: position{ pos }, velocity{ vel }, acceleration{ Vector{ 0,0,0 } }, net_force{ Vector{ 0,0,0 } }, mass{mas}, charge{ cha }, radius{ rad }, hardness{ har }, texture{ tex } {}
 
 		Vector position;
@@ -19,7 +17,7 @@ namespace phys
 		double charge;
 		double radius;
 		double hardness;
-		SDL_Texture* texture;
+		int texture;
 
 		static void Electrify(Particle& p1, Particle& p2);
 		static void Gravitate(Particle& p1, Particle& p2);
