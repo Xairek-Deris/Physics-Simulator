51d0c9045c595721b03bc0ac5cee0257bace5675
Xairek-Deris <xairek@outlook.com>
2021-03-30 20:32:02 -0400

    Revamped main to work with changes, small demonstration is hard coded in until i can get the rest made

--- Main.cpp
+++ Main.cpp
@@ -2,14 +2,16 @@
 #include <vector>
 #include <unordered_map>
 
+#include <SDL2/SDL.h>
+
 #include "Window.h"
 #include "Clock.h"
 #include "Particle.h"
 #include "Vec.h"
-#include "Init_SDL.h"
 #include "Space.h"
 #include "Engine.h"
 #include "Display.h"
+#include "Camera.h"
 
 namespace phys
 {
@@ -19,6 +21,8 @@ namespace phys
     enum class cmd
     {
         INVALID,
+        PAUSE,
+        UNPAUSE,
         LOAD_SPACE,
         SAVE_SPACE,
         NEW,
@@ -34,6 +38,8 @@ namespace phys
     {
         {"load", cmd::LOAD_SPACE},
         {"save", cmd::SAVE_SPACE},
+        {"pause", cmd::PAUSE},
+        {"unpause", cmd::UNPAUSE},
         {"new", cmd::NEW},
         {"delete", cmd::DELETE}, 
         {"edit", cmd::EDIT},
@@ -46,18 +52,28 @@ namespace phys
 
 int main(int argc, char** argv)
 {
-    phys::Init();
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
     phys::Window window("Simulation", 1280, 1280);
+    phys::Display display(window);
     phys::Space space;
-    phys::Engine engine(&space);
+    phys::Engine engine(space);
+    space.particles.push_back(phys::Particle{ phys::Vector{ 0,0,0 }, phys::Vector{ 1,0,0}, 10, 1, 1, 1, 0});
+    space.cameras.push_back( phys::Camera(phys::Vector{0, 0, 0}, phys::Vector{ 0, 1, 0 }, 100) );
+    display.New_Texture("ball.bmp");
     engine.Start();
-    phys::Display display(window, space, "ball.bmp");
-    display.Start();
     while(!window.Should_Quit())
     {
         window.Poll_Events();
+        display.Clear();
+        display.Draw_Particles(space.particles, space.cameras[0]);
+        display.Update();
+        phys::dframes++;
     }
     engine.Stop();
-    display.Stop();
     std::cout << phys::pframes/phys::dframes << std::endl;
 }
