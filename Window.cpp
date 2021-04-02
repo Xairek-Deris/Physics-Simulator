98e2dbde021a38e4c444140e5dc214fd61a15b7d
Xairek-Deris <xairek@outlook.com>
2021-04-01 20:50:06 -0400

    Moved quit logic into window and added m_quit member

--- Window.cpp
+++ Window.cpp
@@ -6,7 +6,11 @@ int phys::Window::Poll_Event()
 {
     if(SDL_PollEvent(&m_event) != 0)
     {
-        if (m_event.type == SDL_WINDOWEVENT)
+        if(m_event.type == SDL_MOUSEMOTION || m_event.type == SDL_MOUSEBUTTONDOWN || m_event.type == SDL_MOUSEBUTTONUP)
+        {
+            m_mouse_state = SDL_GetMouseState(&m_mouse_x, &m_mouse_y);
+        }
+        else if(m_event.type == SDL_WINDOWEVENT)
         {
             if(m_event.window.event == SDL_WINDOWEVENT_RESIZED)
             {
@@ -14,10 +18,8 @@ int phys::Window::Poll_Event()
                 m_height = m_event.window.data2;
             }
         }
-        else if(m_event.type == SDL_MOUSEMOTION || m_event.type == SDL_MOUSEBUTTONDOWN || m_event.type == SDL_MOUSEBUTTONUP)
-        {
-            m_mouse_state = SDL_GetMouseState(&m_mouse_x, &m_mouse_y);
-        }
+        else if(m_event.type == SDL_QUIT)
+            m_quit = true;
         return 1;
     }
     return 0;
--- Window.h
+++ Window.h
@@ -10,7 +10,7 @@ namespace phys
     {
     public:
         Window(const std::string& title, int width, int height)
-        : m_width{ width }, m_height{ height }
+        : m_width{ width }, m_height{ height }, m_quit { false }
         {
             m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
             m_keystates = SDL_GetKeyboardState( NULL );
@@ -25,12 +25,14 @@ namespace phys
         int Mouse_Y() const { return m_mouse_y; }
         bool Button_Is_Pressed(int scancode) { return m_keystates[scancode]; }
         bool MB_Is_Pressed(int button) { return m_mouse_state & SDL_BUTTON(button); }
+        bool Should_Quit() { return m_quit; }
         const SDL_Event& Event() { return m_event; }
         SDL_Window* Get_Window() { return m_window; }
 
     private:
         int m_width, m_height;
         int m_mouse_x, m_mouse_y;
+        bool m_quit;
         SDL_Window* m_window;
         const Uint8* m_keystates;
         Uint32 m_mouse_state;
