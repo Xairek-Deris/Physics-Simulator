64f814ac64d63ffca20e010ec1a5a2c598101e5b
Xairek-Deris <xairek@outlook.com>
2021-04-01 21:47:28 -0400

    More optimization to particle interaction

--- Particle.cpp
+++ Particle.cpp
@@ -10,16 +10,12 @@ void phys::Particle::Interact(Particle& p1, Particle& p2)
 		return;
 	dist /= mag;															//normalized distance vector
 	Vector force = dist * 6.67408e-11 * p1.mass * p2.mass / (mag * mag);
-	p1.net_force += force;
-	p2.net_force -= force;
-	force = dist * 8.987551787e9 * p1.charge * p2.charge / (mag * mag);		//Coulomb's constant
-	p1.net_force += force;
-	p2.net_force -= force;
+	force += dist * 8.987551787e9 * p1.charge * p2.charge / (mag * mag);		//Coulomb's constant
 	double depth = p1.radius + p2.radius - mag;
 	if(depth > 0)
 	{
-		force = dist * 1e6 * depth * (p1.hardness < p2.hardness ? p1.hardness : p2.hardness);
-		p1.net_force += force;
-		p2.net_force -= force;
+		force += dist * 1e6 * depth * (p1.hardness < p2.hardness ? p1.hardness : p2.hardness);
 	}
+	p1.net_force += force;
+	p2.net_force -= force;
 }
