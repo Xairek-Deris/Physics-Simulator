e016be5f7250f46fdde1eebe54077eed85a96ea5
Xairek-Deris <xairek@outlook.com>
2021-04-05 20:13:31 -0400

    Fixed segfault in the loop

--- Events.cpp
+++ Events.cpp
@@ -7,10 +7,14 @@ int phys::Event_Loop::Function(void* e_loop)
     #define LOOP (*(Event_Loop*)e_loop)
     while(!LOOP.m_stop)
     {
-        LOOP.m_mutex.lock();
-        Event e = LOOP.m_events.front();
-        LOOP.m_events.pop();
-        LOOP.m_mutex.unlock();
-        e.Handler(e.data);
+        if(!LOOP.m_events.empty())
+        {
+            LOOP.m_mutex.lock();
+            Event e = LOOP.m_events.front();
+            LOOP.m_events.pop();
+            LOOP.m_mutex.unlock();
+            e.Handler(e.data);
+        }
     }
+    return 0;
 }
