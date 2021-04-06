92194aaad1f69b703ed85ef2e3cf8e5886d49928
Xairek-Deris <xairek@outlook.com>
2021-04-05 22:17:20 -0400

    Simplified button and updated readme

--- Button.h
+++ Button.h
@@ -12,23 +12,13 @@ namespace phys
     class Button
     {
     public:
-        Button(const Texture& tex_def, const Texture& tex_press, const Texture& tex_dis, const Texture& tex_hov, const Box& box, Event_Loop& e_loop, void (*handler)(void*), void* data, bool shown, bool enabled)
-        : m_texture_default{ &tex_def }, m_texture_pressed{ &tex_press }, m_texture_disabled{ &tex_dis }, m_texture_hovered{ &tex_dis }, m_box{ box }, m_event_loop{ &e_loop }, m_on_click{ handler }, m_data{ data }, 
-            m_pressed{ false }, m_shown{ shown }, m_enabled{ enabled }, m_hovered{ false }  {}
+        Button(const Texture& tex, const Box& box, Event_Loop& e_loop, void (*handler)(void*), void* data, bool shown, bool enabled)
+        : m_texture{ &tex }, m_box{ box }, m_event_loop{ &e_loop }, m_on_click{ handler }, m_data{ data }, m_pressed{ false }, m_shown{ shown }, m_enabled{ enabled } {}
 
         void Click() { m_event_loop->Push(m_on_click, m_data); }
         void MB1Down(const Point& point) { if(m_enabled && m_box.Contains(point)) m_pressed = true; }
         void MB1Up(const Point& point) { if(m_pressed && m_box.Contains(point)) Click(); m_pressed = false; }
-        void Hover(const Point& point) { if(m_box.Contains(point)) m_hovered = true; else m_hovered = false; }
-        void Paint() 
-        {
-            if(m_shown)
-            {
-                if(m_pressed) 
-                    m_texture_pressed->Draw(m_box.Get_SDL_Rect());
-                else m_texture_default->Draw(m_box.Get_SDL_Rect());
-            }
-        }
+        void Paint() { if(m_shown) m_texture->Draw(m_box.Get_SDL_Rect()); }
         bool Is_Shown() { return m_shown; }
         void Show() { m_shown = true; }
         void Hide() { m_shown = false; }
@@ -37,10 +27,7 @@ namespace phys
         void Disable() { m_enabled = false; }
 
     private:
-        const Texture* m_texture_default;
-        const Texture* m_texture_pressed;
-        const Texture* m_texture_disabled;
-        const Texture* m_texture_hovered;
+        const Texture* m_texture;
         Box m_box;
         Event_Loop* m_event_loop;
         void (*m_on_click)(void* data);
@@ -48,6 +35,5 @@ namespace phys
         bool m_pressed;
         bool m_shown;
         bool m_enabled;
-        bool m_hovered;
     };
 }
--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 Small physics simulator that is my first start-to-finish project. 
 Will be practicing c++, and specifically object oriented programming,
 as well as refreshing some basic physics that I learned in school.
+
+I intentionally avoided calculus in my engine so I could focus on the programming side of things.
+I also wanted to test the limits of doing physics with rapid frame updates instead of calculus.
+
+REQUIREMENTS:
+SDL runtime binaries and libraries are needed as I do not have the libraries included in the project files yet.
+
+Currently only has hard-coded particles. You can add some more by using pushing back particles into the "particles" vector.
+Obstacles are just particles that never move. You create one by pushing a particle into the "obstacles" vector.
+
+The program quickly becomes less accurate if your particles move too fast and/or there are too many particles. This is because 
+the physics is done on a separate thread without calculus in the calculations. This means that high update rates are required to
+simulate a continuous calculation like calculus would give. Under small ( < 10 ) numbers of particles, with low ( < 20 ) speeds,
+the simulation is pretty accurate.
+
+Program only renders 2d because I do not know how to use OpenGL yet. The physics are in 3d, however, so if you set a particle to
+Z!=0 then it will work properly, but the view will be silhouettes of the actual motion.
+
+GUI portions are still unimplemented because I am building one by scratch so I can learn how they work.
